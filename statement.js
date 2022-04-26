ebc4756ac1095ad2bf343ff5a21c2b3774094033
Valentina <maggio.vale@gmail.com>
2022-04-26 11:15:32 +0100

    Statement class with methods to print header and transactions

--- statement.js
+++ statement.js
@@ -1,6 +1,27 @@
 class Statement {
-  getHeader() {
-    return 'date || credit || debit || balance';
+  constructor() {
+    this.statement = '';
+    this.header = 'date || credit || debit || balance';
+  }
+
+  printHeader() {
+    return this.header;
+  }
+
+  printTransactions(transactions) {
+    transactions.forEach((transaction) => {
+      this.#transactionFormatting(transaction);
+    });
+    this.statement = transactions.reverse().join('\n');
+    return this.statement;
+  }
+
+  #transactionFormatting(transaction) {
+    if (transaction[0] === 'deposit') {
+      transaction.splice(0, 4, `${transaction[1]} || ${transaction[2]} || || ${transaction[3]}`);
+    } if (transaction[0] === 'withdrawal') {
+      transaction.splice(0, 4, `${transaction[1]} || || ${transaction[2]} || ${transaction[3]}`);
+    }
   }
 }
 
--- statement.test.js
+++ statement.test.js
@@ -10,6 +10,16 @@ describe('Statement', () => {
   it('returns the document header', () => {
     const statement = new Statement();
 
-    expect(statement.getHeader()).toBe('date || credit || debit || balance');
+    expect(statement.printHeader()).toBe('date || credit || debit || balance');
+  });
+
+  it('returns a list of transactions', () => {
+    const statement = new Statement();
+    const transactions = [
+      ['deposit', '22/04/2022', 150, 150],
+      ['deposit', '23/04/2022', 200, 350],
+      ['withdrawal', '25/04/2022', 50, 300]];
+
+    expect(statement.printTransactions(transactions)).toStrictEqual('25/04/2022 || || 50 || 300\n23/04/2022 || 200 || || 350\n22/04/2022 || 150 || || 150');
   });
 });
