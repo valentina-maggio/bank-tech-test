0cb4d109ddd0603d19e3e589600215f7275a4e49
Valentina <maggio.vale@gmail.com>
2022-04-26 17:21:46 +0100

    Add Statement method to format numbers to two decimals

--- statement.js
+++ statement.js
@@ -17,11 +17,17 @@ class Statement {
   }
 
   #transactionFormatting(transaction) {
+    this.#formatNumber(transaction);
     if (transaction[0] === 'deposit') {
       transaction.splice(0, 4, `${transaction[1]} || ${transaction[2]} || || ${transaction[3]}`);
     } if (transaction[0] === 'withdrawal') {
       transaction.splice(0, 4, `${transaction[1]} || || ${transaction[2]} || ${transaction[3]}`);
     }
+  }
+
+  #formatNumber(transaction) {
+    transaction[2] = Number(transaction[2]).toFixed(2);
+    transaction[3] = Number(transaction[3]).toFixed(2);
   }
 }
 
