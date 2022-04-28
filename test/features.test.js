4518d110e0bdd0b72afedc8a25b7d5620d402b16
Valentina <maggio.vale@gmail.com>
2022-04-28 22:30:12 +0100

    Update features test

--- test/features.test.js
+++ test/features.test.js
@@ -1,31 +1,24 @@
 const BankAccount = require('../src/bankAccount');
 
-const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
-
 describe('Feature test', () => {
-  beforeEach(() => {
-    consoleSpy.mockClear();
-  });
-
-  it('implements the acceptance criteria requested by the client', () => {
-    const fakeDate1 = { format: '10/01/2023' };
-    const fakeDate2 = { format: '13/01/2023' };
-    const fakeDate3 = { format: '14/01/2023' };
+  it('implements all the operations for the account management', () => {
+    Date.now = jest.fn(() => new Date('2022-04-27T12:33:37.000Z'));
 
     const account = new BankAccount();
 
-    account.deposit(1000, fakeDate1.format);
-    account.deposit(2000, fakeDate2.format);
-    account.withdraw(500, fakeDate3.format);
+    account.deposit(1000);
+    account.deposit(2000);
+    account.withdraw(500);
+
+    console.log = jest.fn();
+
     account.viewStatement();
 
-    // Commenting out the below code as the Spy only returns 'Number of calls: 2'
-
-    // expect(console.log).toHaveBeenCalledWith(
-    //   'date || credit || debit || balance\n'
-    //   + '14/01/2023 || || 500.00 || 2500.00\n'
-    //   + '13/01/2023 || 2000.00 || || 3000.00\n'
-    //   + '14/01/2023 || 1000.00 || || 1000.00',
-    // );
+    expect(console.log).toHaveBeenCalledWith(
+      'date || credit || debit || balance\n'
+      + '27/04/2022 || || 500.00 || 2500.00\n'
+      + '27/04/2022 || 2000.00 || || 3000.00\n'
+      + '27/04/2022 || 1000.00 || || 1000.00',
+    );
   });
 });
